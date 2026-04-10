63c39c8a799148bd25e643a571c5274108fd9901
nour833 <nourelislem84@outlook.fr>
2026-04-11 01:22:50 +0200

    Clean up audio testing debris files

--- make_dummy.py
+++
@@ -1,7 +0,0 @@
-import wave, struct
-with wave.open("dummy.wav", "w") as f:
-    f.setnchannels(1)
-    f.setsampwidth(2)
-    f.setframerate(44100)
-    for _ in range(44100 * 5):  # 5 seconds
-        f.writeframes(struct.pack('h', 0))
--- test_audio2.py
+++
@@ -1,4 +0,0 @@
-with open("stego.mp3", "rb") as f:
-    data = f.read()
-    print("Does it start with RIFF?", data.startswith(b"RIFF"))
-    
